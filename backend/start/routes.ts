5a48006ddd014ddb131323f5679097addb30f5ca
khalid <sheikhkhalidazad@gmail.com>
2025-12-03 17:48:01 +0600

    feat: reintroduce AuthController and TasksController with full functionality

--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -7,8 +7,19 @@
 |
 */
 
+const TasksController = () => import('#controllers/tasks_controller')
 import router from '@adonisjs/core/services/router'
-const AuthController =()=>import ('#Controllers/Http/auth_controller') from '../app/Controllers/Http/auth_controller.js'
+import { middleware } from './kernel.js'
+// import AuthController from '../app/
+const AuthController = () => import('#controllers/auth_controller')
 
 router.post('/register', [AuthController, 'register'])
-
+router.post('/login', [AuthController, 'login'])
+router
+  .group(() => {
+    router.post('/logout', [AuthController, 'logout'])
+    router.get('/tasks', [TasksController, 'index'])
+    router.post('/tasks', [TasksController, 'store'])
+    router.delete('/task/:id', [TasksController, 'destroy'])
+  })
+  .use(middleware.auth())
