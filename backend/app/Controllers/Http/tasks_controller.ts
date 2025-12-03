7059b4ab99bd71690d969e60cb318d1e347e8daa
khalid <sheikhkhalidazad@gmail.com>
2025-12-03 16:40:18 +0600

    feat: implement task management functionality with index and store methods

--- backend/app/Controllers/Http/tasks_controller.ts
+++ backend/app/Controllers/Http/tasks_controller.ts
@@ -1,3 +1,16 @@
-// import type { HttpContext } from '@adonisjs/core/http'
-
-export default class TasksController {}
+import Task from '#models/task'
+import { HttpContext } from '@adonisjs/core/http'
+export default class TasksController {
+  public async index({ auth }: HttpContext) {
+    return Task.query().where('user_id', auth.user!.id)
+  }
+  public async store({ request, auth }: HttpContext) {
+    const { title, description, status } = request.only(['title', 'description', 'status'])
+    return Task.create({
+      title,
+      description,
+      status: status,
+      userID=auth.user!.id
+    })
+  }
+}
--- backend/app/models/task.ts
+++ backend/app/models/task.ts
@@ -4,10 +4,17 @@ import { BaseModel, column } from '@adonisjs/lucid/orm'
 export default class Task extends BaseModel {
   @column({ isPrimary: true })
   declare id: number
+  @column()
+  declare title: string
+
+  @column()
+  declare description: string
+  @column()
+  declare status: 'todo' | 'in-progress' | 'testing' | 'complete'
+
+  @column()
+  declare userid: number
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
-
-  @column.dateTime({ autoCreate: true, autoUpdate: true })
-  declare updatedAt: DateTime
 }
